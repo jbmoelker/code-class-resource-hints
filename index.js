085d1869d8a62359cb51936179f920bbc4e0d556
Jasper Moelker <j.b.moelker@activeids.nl>
2018-03-14 23:32:23 +0100

    Group exercises into single directory

--- index.js
+++ index.js
@@ -10,11 +10,11 @@ portfinder.basePort = 3000
 app.set('etag', enableCaching)
 app.use('/assets/', express.static(path.join(__dirname, 'src', 'assets'), { etag: enableCaching, lastModified: enableCaching }))
 
-app.get('/', (req, res) => res.send(renderer.render('views/index.html')))
+app.get('/', (req, res) => res.send(renderer.render(path.join('views', 'index.html'))))
 
 app.get('/:page', (req, res) => {
 	const { page } = req.params
-	const filename = path.join('views', page, `${page}.html`)
+	const filename = path.join('views', 'exercises', `${page}.html`)
 	res.send(renderer.render(filename))
 })
 
