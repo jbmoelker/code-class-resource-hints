abeec70841563f7646301790f45560bfdf736e46
Jasper Moelker <j.b.moelker@activeids.nl>
2018-03-15 12:54:33 +0100

    Add exercise 3

--- index.js
+++ index.js
@@ -9,13 +9,15 @@ portfinder.basePort = 3000
 
 app.set('etag', enableCaching)
 app.use('/assets/', express.static(path.join(__dirname, 'src', 'assets'), { etag: enableCaching, lastModified: enableCaching }))
+app.use('/vue/', express.static(path.join(__dirname, 'node_modules', 'vue', 'dist')))
 
 app.get('/', (req, res) => res.send(renderer.render(path.join('views', 'index.html'))))
 
 app.get('/:page', (req, res) => {
 	const { page } = req.params
 	const filename = path.join('views', 'exercises', `${page}.html`)
-	res.send(renderer.render(filename))
+	const timestamp = new Date().getTime()
+	res.send(renderer.render(filename, { timestamp }))
 })
 
 portfinder.getPortPromise().then(port => {
